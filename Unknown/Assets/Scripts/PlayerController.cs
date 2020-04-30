456a6075c10288d5999cb09b6a1995599cdedad5
Nicolas PIccitto <nicolaspiccitto@gmail.com>
2020-04-30 20:16:45 -0300

    Player movement finished

---
+++ Unknown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    Vector3 destination;
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.tag == "Destination")
+                {
+                    destination = hit.point;
+                    player.GetComponent<PlayerMovement>().Walk(destination);
+                }
+
+                if (hit.transform.tag == "Object")
+                {
+
+                }
+
+            }
+
+        }
+    }
+
+}
