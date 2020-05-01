7254160c38418eef28253bc29b490c42c5df48b7
Unknown <li_gueva@hotmail.com>
2020-05-01 00:11:41 -0300

    Camera System Finished

---
+++ Unknown/Assets/Scripts/CameraSystem/CameraChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraChange : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Camera prevCam;
+    [SerializeField] private CameraController cc;
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        if (cc.currentCam == cam)
+        {
+            cc.currentCam = prevCam;
+            cc.CameraEnable();
+        }
+        else
+        {
+            cc.currentCam = cam;
+            cc.CameraEnable();
+        }
+    }
+        
+}
