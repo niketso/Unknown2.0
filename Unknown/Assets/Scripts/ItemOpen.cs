699e41713cccc3adb96ba4f360c9d99284705384
Nicolas PIccitto <nicolaspiccitto@gmail.com>
2020-07-26 17:09:28 -0300

    Hospital Scene added.

---
+++ Unknown/Assets/Scripts/ItemOpen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemOpen : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        Open();
+    }
+
+    public void Open()
+    {
+        Debug.Log("OPEN");
+        if (this.GetComponentInParent<Animator>().GetBool("IsLocked") == false)
+            this.GetComponentInParent<Animator>().SetTrigger("OpenDoor");
+    }
+}
