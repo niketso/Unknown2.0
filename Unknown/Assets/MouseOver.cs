d1e7f42ed55c7606f56e8a382dc189e59c3caf33
Unknown <li_gueva@hotmail.com>
2020-08-03 17:01:35 -0300

    Pop Up with mouseOver created

---
+++ Unknown/Assets/MouseOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOver : MonoBehaviour
+{
+    [SerializeField]
+    GameObject popUpWindow;
+
+    PopUpController popUpController;
+    public string text;
+    void Start()
+    {
+        text = GetComponent<Text>().text;
+        popUpController = popUpWindow.GetComponent<PopUpController>();
+    }
+
+    void OnMouseOver()
+    {
+        popUpWindow.SetActive(true);
+        popUpController.itemText = text;        
+    }
+
+    void OnMouseExit()
+    {
+        popUpWindow.SetActive(false);
+    }
+}
---
+++ Unknown/Assets/PopUpController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpController : MonoBehaviour
+{
+    public string itemText;
+
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {     
+        transform.Find("Text").GetComponent<Text>().text = itemText; 
+    }
+    
+
+}
