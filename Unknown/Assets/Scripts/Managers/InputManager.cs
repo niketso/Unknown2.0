969362a62a2ce72e5518716996fcc5aa91260a0b
Nicolas PIccitto <nicolaspiccitto@gmail.com>
2020-11-24 20:02:17 -0300

    InputManager added.

---
+++ Unknown/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<InputManager>();
+            }
+            return instance;
+        }
+
+    }
+
+    private void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+      
+    }
+
+    public void UnlockMouse()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LockMouse()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
